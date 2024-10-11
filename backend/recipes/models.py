4cfed06dca78dd7d754d9284b273efff9da7f50e
useyourcode <154615654+useyourcode@users.noreply.github.com>
2024-10-11 12:22:58 +0300

    fix import2

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,9 @@
-from colorfield.fields import ColorField
-from django.core.validators import (MaxValueValidator, MinValueValidator,
-                                    RegexValidator)
+from django.core.validators import (
+    MaxValueValidator, MinValueValidator, RegexValidator
+)
 from django.db import models
+
+from colorfield.fields import ColorField
 from users.models import User
 
 
