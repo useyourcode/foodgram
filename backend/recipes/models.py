44a1b968b1e0c7bfbf81abdade562d7fc796edc5
useyourcode <154615654+useyourcode@users.noreply.github.com>
2024-10-12 16:43:51 +0300

    fix import4

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,8 +2,8 @@ from django.core.validators import (
     MaxValueValidator, MinValueValidator, RegexValidator
 )
 from django.db import models
-
 from colorfield.fields import ColorField
+
 from users.models import User
 
 
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,5 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 
 
