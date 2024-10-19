2e0f6c55de459f12aef420e966d5ed8f7a9f565c
useyourcode <154615654+useyourcode@users.noreply.github.com>
2024-10-19 16:31:33 +0300

    fix subscribe

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -94,7 +94,7 @@ class SubscribeListSerializer(djoser.serializers.UserSerializer):
         author_id = request.parser_context.get('kwargs').get('id')
         author = get_object_or_404(User, id=author_id)
         user = request.user
-        if Subscription.filter(subscriber=user, author=author).exists():
+        if Subscription.objects.filter(subscriber=user, author=author).exists():
             raise serializers.ValidationError(
                 'Вы уже подписаны',
                 code=status.HTTP_400_BAD_REQUEST
