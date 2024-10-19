15fd71c8ef32285783ccd5d7569c9a5d28c72314
useyourcode <154615654+useyourcode@users.noreply.github.com>
2024-10-19 19:13:52 +0300

    fix subscribe

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,4 @@
 import djoser.serializers
-from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
