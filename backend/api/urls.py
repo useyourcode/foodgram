f88af34e58ab4fcc25b55ef3a8a05672e487cac0
useyourcode <154615654+useyourcode@users.noreply.github.com>
2024-10-21 00:40:19 +0300

    fix subscribe

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('users/subscriptions/', UserViewSet.as_view({'get': 'subscriptions'}), name='subscriptions'),
 ]
